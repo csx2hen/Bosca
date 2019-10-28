48c2022897eb458b9839f869295075facc92c1e3
csx2hen <csx2hen@gmail.com>
2019-10-28 10:01:20 +0000

    Add function of creating a new user to user service.

--- user/src/main/java/com/bosca/user/controllers/UserController.java
+++ user/src/main/java/com/bosca/user/controllers/UserController.java
@@ -1,15 +1,41 @@
 package com.bosca.user.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bosca.user.models.CreateUserRequestModel;
+import com.bosca.user.models.CreateUserResponseModel;
+import com.bosca.user.services.UserService;
+import com.bosca.user.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping
-    public String status() {
-        return "Working";
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        UserDto createdUser = userService.createUser(userDto);
+
+        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 }
---
+++ user/src/main/java/com/bosca/user/services/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.bosca.user.services;
+
+import com.bosca.user.data.UserEntity;
+import com.bosca.user.data.UserRepository;
+import com.bosca.user.shared.UserDto;
+import org.apache.catalina.User;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+
+        userDetails.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+        userRepository.save(userEntity);
+
+        return modelMapper.map(userEntity, UserDto.class);
+    }
+}
