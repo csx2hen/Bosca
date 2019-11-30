0516fc0ae7c5c8a33fb3f6b0cd05d6aadf1312fd
csx2hen <csx2hen@gmail.com>
2019-11-30 19:02:35 +0000

    Init the module of metadata service and add dependency.

---
+++ metadata-ws/src/main/java/com/bosca/metadata/MetadataWsApplication.java
@@ -0,0 +1,13 @@
+package com.bosca.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetadataWsApplication.class, args);
+    }
+
+}
---
+++ metadata-ws/src/test/java/com/bosca/metadata/MetadataWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosca.metadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
